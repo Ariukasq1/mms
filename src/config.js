7efd7f15b372a925e589b0ec21d514643d5066a5
anu-ujin <anu.b@nmma.co>
2021-07-06 15:28:22 +0800

    update config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const Config = {
-  apiUrl: "http://www.source.mn//wp/wp-json",
-  menuUrl: "http://www.source.mn//wp/wp-json/menus/v1/menus",
+  apiUrl: "http://source.mn/wp/wp-json",
+  menuUrl: "http://source.mn/wp/wp-json/menus/v1/menus",
 };
 
 export const generateLink = (url) => {
