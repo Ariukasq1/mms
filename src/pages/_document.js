a79f05549112c7228ec7bacace0d94449682171b
anu-ujin <anu.b@nmma.co>
2021-07-07 10:55:16 +0800

    some update

---
+++ src/pages/_document.js
@@ -0,0 +1,55 @@
+import Document, { Html, Main, NextScript } from "next/document";
+import React from "react";
+import { getLangParam } from "../utils";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang={getLangParam()}>
+        <body style={{ margin: 0 }}>
+          {/* TagManager */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MPQD53D"
+                        height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+          {/* FB chat */}
+          <div id="fb-root"></div>
+          <div id="fb-customer-chat" className="fb-customerchat">
+            {" "}
+          </div>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `var chatbox = document.getElementById('fb-customer-chat');
+                        chatbox.setAttribute("page_id", "1043670075778655");
+                        chatbox.setAttribute("attribution", "biz_inbox");
+                        window.fbAsyncInit = function() {
+                          FB.init({
+                            xfbml            : true,
+                            version          : 'v11.0'
+                          });
+                        };
+
+                        (function(d, s, id) {
+                          var js, fjs = d.getElementsByTagName(s)[0];
+                          if (d.getElementById(id)) return;
+                          js = d.createElement(s); js.id = id;
+                          js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                          fjs.parentNode.insertBefore(js, fjs);
+                        }(document, 'script', 'facebook-jssdk'));`,
+            }}
+          />
+
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
