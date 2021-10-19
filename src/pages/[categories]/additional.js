9d0caf1b314250a6c01839679d977d8c1b459b7a
Bulgantamir <bulgantamir@nmma.co>
2021-10-19 16:47:31 +0800

    little fix

--- src/pages/[categories]/additional.js
+++ src/pages/[categories]/additional.js
@@ -9,7 +9,7 @@ class Additional extends React.Component {
         data-aos-delay={`${index * 100}`}
         data-aos-duration="2000"
         data-aos-offset="300"
-        className="h-body w-full p-10 content text-base relative overflow-auto sm:h-auto lg:pl-16 xl:p-5 lg:pr-4 md:px-10 sm:pl-0 sm:pr-0 sm:pt-5 sm:px-8 md:h-auto lg:h-auto"
+        className="h-body w-full p-10 content text-base relative overflow-auto sm:h-auto lg:pl-16 xl:p-5 lg:pr-4 md:px-10 sm:pt-5 sm:px-8 md:h-auto lg:h-auto"
       >
         <img
           className="mb-2"
